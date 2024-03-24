e193b4b076a4b741b260bec19a437aaefa1567ad
Anwarul07 <mdanwarul064@gmail.com>
2024-03-24 12:02:19 +0530

    Update Index.html

---
+++ windows.js
@@ -0,0 +1,8 @@
+console.log("welcome to windows 11");
+document.querySelector(".taskbar").addEventListener("click", ()=>{
+    document.querySelector(".start").classList.toggle("opacity");
+
+    console.log("Taskbar opened");
+});
+
+
